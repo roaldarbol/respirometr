0023075b0b68231c5ebeafe23b9189fb34a0e7e8
Mikkel Roald-Arbøl <github.ggb9a@simplelogin.co>
2024-04-07 22:38:11 +0100

    First commit

---
+++ R/normalise.R
@@ -0,0 +1,5 @@
+normalise_prepost <- function(data, data_pre, data_post){
+  cols = c("co2d_um_m", "h2od_dp_c", "h2od_mm_m")
+  data_pre
+
+}
---
+++ R/read_licor.R
@@ -0,0 +1,26 @@
+#' Read Licor data
+#'
+#' @param filepath
+#' @param model
+#'
+#' @importFrom janitor make_clean_names
+#' @importFrom readr read_tsv
+#' @importFrom purrr map
+#' @importFrom dplyr mutate_all
+#'
+#' @return
+#' @export
+#'
+read_licor <- function(filepath, model = "LI7000") {
+  if (model == "LI7000"){
+    df <- filenames |>
+      purrr::map(~ read_tsv(.x, skip = 2, name_repair = janitor::make_clean_names))
+  }
+
+  # Avoids issues when column names have been loaded partway through
+  df <- df |>
+    na.omit() |>
+    dplyr::mutate_all(as.numeric)
+
+  return(df)
+}
