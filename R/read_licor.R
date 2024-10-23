f505057f70d12dde6fcf5211a050ebc9a7de48ce
Mikkel Roald-Arbøl <github.ggb9a@simplelogin.co>
2024-10-23 22:26:07 +0200

    Updated functions and man

--- R/read_licor.R
+++ R/read_licor.R
@@ -1,21 +1,26 @@
 #' Read Licor data
 #'
+#' @description
+#' `r lifecycle::badge('experimental')`
+#'
+#' Read respirometry data from a variety of Licor devices.
+#'
 #' @param filepath Path to file
 #' @param model LiCor model
 #'
+#' @import dplyr
 #' @importFrom janitor make_clean_names
 #' @importFrom readr read_tsv
 #' @importFrom purrr map
-#' @importFrom dplyr mutate_all
 #' @importFrom stats na.omit
 #'
 #' @return Tidy LiCor data
 #' @export
-#'
-read_licor <- function(filepath, model = "LI7000") {
-  if (model == "LI7000"){
-    df <- filepath |>
-      purrr::map(~ read_tsv(.x, skip = 2, name_repair = janitor::make_clean_names))
+read_licor <- function(filepath, model = "7000") {
+  if (model == "7000"){
+    df <- read_li7000(filepath)
+  } else if (model == "850"){
+    df <- read_li850(filepath)
   }
 
   # Avoids issues when column names have been loaded partway through
@@ -24,4 +29,24 @@ read_licor <- function(filepath, model = "LI7000") {
     dplyr::mutate_all(as.numeric)
 
   return(df)
+}
+
+#' Read from LI-7000
+#' @inheritParams read_licor
+read_li7000 <- function(filepath){
+  filepath |>
+    read_tsv(skip = 2, name_repair = janitor::make_clean_names)
+}
+
+#' Read from LI-7000
+#' @inheritParams read_licor
+read_li850 <- function(filepath){
+  filepath |>
+    read_tsv(skip = 1,
+             name_repair = janitor::make_clean_names,
+             show_col_types = FALSE) |>
+    suppressWarnings() |>
+    dplyr::select(where(.not_any_na)) |>
+    dplyr::mutate(time_s = .data$system_time_h_m_s - min(.data$system_time_h_m_s)) |>
+    dplyr::relocate("time_s", .before = 1)
 }
---
+++ R/utils.R
@@ -0,0 +1,2 @@
+.not_all_na <- function(x) any(!is.na(x))
+.not_any_na <- function(x) all(!is.na(x))
